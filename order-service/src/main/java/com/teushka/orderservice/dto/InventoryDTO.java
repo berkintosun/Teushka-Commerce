8bd7ee222e851439cf3e2ad83719732675ade154
Berkin Tosun <berkintsn@gmail.com>
2023-01-22 23:16:17 +0200

    Updating Order microservice to communicate with inventory microservice

---
+++ order-service/src/main/java/com/teushka/orderservice/dto/InventoryDTO.java
@@ -0,0 +1,15 @@
+package com.teushka.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InventoryDTO {
+    private String skuCode;
+    private boolean isInStock;
+}
--- order-service/src/main/java/com/teushka/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/teushka/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.teushka.orderservice.service;
 
+import com.teushka.orderservice.dto.InventoryDTO;
 import com.teushka.orderservice.dto.OrderItemDTO;
 import com.teushka.orderservice.dto.OrderRequest;
 import com.teushka.orderservice.entity.Order;
@@ -7,7 +8,10 @@ import com.teushka.orderservice.entity.OrderItem;
 import com.teushka.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,8 +20,11 @@ public class OrderService {
 
     private OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private WebClient webClient;
+
+    public OrderService(OrderRepository orderRepository, WebClient webClient) {
         this.orderRepository = orderRepository;
+        this.webClient = webClient;
     }
 
     public void placeOrder(OrderRequest orderRequest){
@@ -28,7 +35,20 @@ public class OrderService {
                 .stream()
                 .map(this::mapToOrderItem)
                 .toList());
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderItems().stream().map(OrderItem::getSkuCode).toList();
+        InventoryDTO[] inventoryDTOS = webClient.get()
+                .uri("http://127.0.0.1:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build()
+                )
+                .retrieve()
+                .bodyToMono(InventoryDTO[].class)
+                .block();
+
+        Boolean allItemsInStock = Arrays.stream(inventoryDTOS).allMatch(InventoryDTO::isInStock);
+        if(!allItemsInStock){
+            throw new IllegalArgumentException("Product is not in stock.");
+        }
+            orderRepository.save(order);
     }
 
     private OrderItem mapToOrderItem(OrderItemDTO dto) {
